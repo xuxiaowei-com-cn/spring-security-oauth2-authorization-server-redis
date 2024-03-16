3b3b7f3107711dbf76d24e83cab1e320e5fbb0c5
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-03-16 16:00:25 +0800

    :sparkles: oauth2_authorization_consent 表 Redis 实现

--- src/main/java/cn/com/xuxiaowei/boot/oauth2/service/RedisOAuth2AuthorizationConsentService.java
+++ src/main/java/cn/com/xuxiaowei/boot/oauth2/service/RedisOAuth2AuthorizationConsentService.java
@@ -1,6 +1,16 @@
 package cn.com.xuxiaowei.boot.oauth2.service;
 
+import cn.com.xuxiaowei.boot.oauth2.constant.RedisConstants;
+import cn.com.xuxiaowei.boot.oauth2.deserializer.OAuth2AuthorizationConsentDeserializer;
 import cn.com.xuxiaowei.boot.oauth2.properties.SpringAuthorizationServerRedisProperties;
+import cn.com.xuxiaowei.boot.oauth2.utils.RedisUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
@@ -8,10 +18,13 @@ import org.springframework.security.oauth2.server.authorization.OAuth2Authorizat
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author xuxiaowei
  * @since 2.0.0
  */
+@Slf4j
 public class RedisOAuth2AuthorizationConsentService implements OAuth2AuthorizationConsentService {
 
 	private final JdbcOAuth2AuthorizationConsentService jdbcOAuth2AuthorizationConsentService;
@@ -20,6 +33,10 @@ public class RedisOAuth2AuthorizationConsentService implements OAuth2Authorizati
 
 	private final SpringAuthorizationServerRedisProperties properties;
 
+	@Setter
+	@Getter
+	private ObjectMapper objectMapper = new ObjectMapper();
+
 	public RedisOAuth2AuthorizationConsentService(JdbcOperations jdbcOperations,
 			RegisteredClientRepository registeredClientRepository, StringRedisTemplate stringRedisTemplate,
 			SpringAuthorizationServerRedisProperties properties) {
@@ -27,21 +44,91 @@ public class RedisOAuth2AuthorizationConsentService implements OAuth2Authorizati
 				registeredClientRepository);
 		this.stringRedisTemplate = stringRedisTemplate;
 		this.properties = properties;
+
+		SimpleModule simpleModule = new SimpleModule();
+		simpleModule.addDeserializer(OAuth2AuthorizationConsent.class, new OAuth2AuthorizationConsentDeserializer());
+
+		objectMapper.registerModule(simpleModule);
 	}
 
+	@SneakyThrows
 	@Override
 	public void save(OAuth2AuthorizationConsent authorizationConsent) {
 		jdbcOAuth2AuthorizationConsentService.save(authorizationConsent);
+
+		long timeout = properties.getAuthorizationConsentTimeout();
+
+		String json = objectMapper.writeValueAsString(authorizationConsent);
+
+		String registeredClientId = authorizationConsent.getRegisteredClientId();
+		String principalName = authorizationConsent.getPrincipalName();
+
+		String key = key(registeredClientId, principalName);
+
+		stringRedisTemplate.opsForValue().set(key, json, timeout, TimeUnit.SECONDS);
 	}
 
 	@Override
 	public void remove(OAuth2AuthorizationConsent authorizationConsent) {
 		jdbcOAuth2AuthorizationConsentService.remove(authorizationConsent);
+
+		String registeredClientId = authorizationConsent.getRegisteredClientId();
+		String principalName = authorizationConsent.getPrincipalName();
+
+		String key = key(registeredClientId, principalName);
+
+		stringRedisTemplate.delete(key);
 	}
 
+	@SneakyThrows
 	@Override
 	public OAuth2AuthorizationConsent findById(String registeredClientId, String principalName) {
-		return jdbcOAuth2AuthorizationConsentService.findById(registeredClientId, principalName);
+
+		String key = key(registeredClientId, principalName);
+
+		long timeout = properties.getAuthorizationConsentTimeout();
+
+		String redisVersion = RedisUtils.redisVersion(stringRedisTemplate);
+		int compare = StringUtils.compare(redisVersion, RedisConstants.GETDEL_VERSION);
+
+		String json;
+
+		if (compare < 0) {
+			log.warn("警告：Redis 版本低于 {}，不支持 GETEX（getAndExpire）命令", RedisConstants.GETEX_VERSION);
+
+			json = stringRedisTemplate.opsForValue().get(key);
+
+			if (json != null) {
+				stringRedisTemplate.expire(key, timeout, TimeUnit.SECONDS);
+			}
+		}
+		else {
+			json = stringRedisTemplate.opsForValue().getAndExpire(key, timeout, TimeUnit.SECONDS);
+		}
+
+		OAuth2AuthorizationConsent oauth2AuthorizationConsent;
+
+		if (json == null) {
+			oauth2AuthorizationConsent = jdbcOAuth2AuthorizationConsentService.findById(registeredClientId,
+					principalName);
+
+			if (oauth2AuthorizationConsent != null) {
+
+				json = objectMapper.writeValueAsString(oauth2AuthorizationConsent);
+
+				stringRedisTemplate.opsForValue().set(key, json, timeout, TimeUnit.SECONDS);
+			}
+		}
+		else {
+			oauth2AuthorizationConsent = objectMapper.readValue(json, OAuth2AuthorizationConsent.class);
+		}
+
+		return oauth2AuthorizationConsent;
+	}
+
+	public String key(String registeredClientId, String principalName) {
+		String prefix = properties.getPrefix();
+		return prefix + ":oauth2_authorization_consent:" + registeredClientId + ":" + principalName;
 	}
 
 }
---
+++ src/test/java/cn/com/xuxiaowei/boot/oauth2/service/RedisOAuth2AuthorizationConsentServiceTests.java
@@ -0,0 +1,79 @@
+package cn.com.xuxiaowei.boot.oauth2.service;
+
+import cn.com.xuxiaowei.boot.oauth2.SpringSecurityOauth2AuthorizationServerRedisApplication;
+import cn.com.xuxiaowei.boot.oauth2.annotation.EnableOAuth2Redis;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsent;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author xuxiaowei
+ * @since 2.0.0
+ */
+@Slf4j
+@EnableOAuth2Redis
+@SpringBootTest(classes = SpringSecurityOauth2AuthorizationServerRedisApplication.class,
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RedisOAuth2AuthorizationConsentServiceTests {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
+	@Autowired
+	private RedisOAuth2AuthorizationConsentService redisOAuth2AuthorizationConsentService;
+
+	@Test
+	void start() throws JsonProcessingException {
+
+		String registeredClientId = UUID.randomUUID().toString();
+		String principalName = UUID.randomUUID().toString();
+
+		OAuth2AuthorizationConsent oauth2AuthorizationConsent = OAuth2AuthorizationConsent
+			.withId(registeredClientId, principalName)
+			.authorities(authoritiesConsumer -> {
+				authoritiesConsumer.add(new SimpleGrantedAuthority("A"));
+				authoritiesConsumer.add(new SimpleGrantedAuthority("B"));
+			})
+			.scope("S1")
+			.build();
+
+		redisOAuth2AuthorizationConsentService.save(oauth2AuthorizationConsent);
+
+		String key = redisOAuth2AuthorizationConsentService.key(registeredClientId, principalName);
+
+		String json = stringRedisTemplate.opsForValue().get(key);
+		assertNotNull(json);
+
+		OAuth2AuthorizationConsent oauth2AuthorizationConsentFindById = redisOAuth2AuthorizationConsentService
+			.findById(registeredClientId, principalName);
+
+		assertEquals(oauth2AuthorizationConsent, oauth2AuthorizationConsentFindById);
+
+		ObjectMapper objectMapper = redisOAuth2AuthorizationConsentService.getObjectMapper();
+
+		OAuth2AuthorizationConsent oauth2AuthorizationConsentByJson = objectMapper.readValue(json,
+				OAuth2AuthorizationConsent.class);
+		assertEquals(oauth2AuthorizationConsent, oauth2AuthorizationConsentByJson);
+
+		redisOAuth2AuthorizationConsentService.remove(oauth2AuthorizationConsent);
+
+		json = stringRedisTemplate.opsForValue().get(key);
+		assertNull(json);
+
+	}
+
+}
