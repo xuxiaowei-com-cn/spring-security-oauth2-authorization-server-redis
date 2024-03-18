6951911e4fb817ae24c01b0fa7de892b471bfb63
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-03-18 09:45:27 +0800

    :white_check_mark: 凭证式：集成测试

--- src/test/java/cn/com/xuxiaowei/boot/oauth2/SpringSecurityOauth2AuthorizationServerRedisApplication.java
+++ src/test/java/cn/com/xuxiaowei/boot/oauth2/SpringSecurityOauth2AuthorizationServerRedisApplication.java
@@ -8,18 +8,79 @@ import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
+import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @SpringBootApplication
+@Configuration(proxyBeanMethods = false)
 public class SpringSecurityOauth2AuthorizationServerRedisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityOauth2AuthorizationServerRedisApplication.class, args);
+	}
+
+	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+			// Enable
+			// OpenID
+			// Connect
+			// 1.0
+			.oidc(Customizer.withDefaults());
+
+		// @formatter:off
+		http
+				.exceptionHandling(exceptions ->
+						exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+				);
+		// @formatter:on
+		return http.build();
+	}
+
+	@Bean
+	public BearerTokenResolver bearerTokenResolver() {
+		DefaultBearerTokenResolver bearerTokenResolver = new DefaultBearerTokenResolver();
+		bearerTokenResolver.setAllowUriQueryParameter(true);
+		return bearerTokenResolver;
+	}
+
+	@Bean
+	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+			.formLogin(withDefaults());
+
+		http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
 	}
 
 	@Bean
