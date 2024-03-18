e6d61bc00bc66a3b2dba8e495e3cd0b4cab05819
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-03-18 09:45:27 +0800

    :white_check_mark: 凭证式：集成测试

--- src/test/java/cn/com/xuxiaowei/boot/oauth2/ClientCredentialsTests.java
+++ src/test/java/cn/com/xuxiaowei/boot/oauth2/ClientCredentialsTests.java
@@ -1,46 +1,49 @@
-package cn.com.xuxiaowei.boot.oauth2.service;
+package cn.com.xuxiaowei.boot.oauth2;
 
-import cn.com.xuxiaowei.boot.oauth2.SpringSecurityOauth2AuthorizationServerRedisApplication;
 import cn.com.xuxiaowei.boot.oauth2.annotation.EnableOAuth2Redis;
+import cn.com.xuxiaowei.boot.oauth2.service.RedisRegisteredClientRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
-import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Collections;
+import java.util.Map;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author xuxiaowei
- * @since 2.0.0
+ * @since 0.0.1
  */
 @Slf4j
 @EnableOAuth2Redis
 @SpringBootTest(classes = SpringSecurityOauth2AuthorizationServerRedisApplication.class,
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class RedisRegisteredClientRepositoryTests {
+class ClientCredentialsTests {
 
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-
-	@Autowired
-	private StringRedisTemplate stringRedisTemplate;
+	@LocalServerPort
+	private int serverPort;
 
 	@Autowired
 	private RedisRegisteredClientRepository redisRegisteredClientRepository;
@@ -65,6 +68,7 @@ public class RedisRegisteredClientRepositoryTests {
 			.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
 			.redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
 			.redirectUri("http://127.0.0.1:8080/authorized")
+			.redirectUri("https://home.baidu.com/home/index/contact_us")
 			.scope(OidcScopes.OPENID)
 			.scope(OidcScopes.PROFILE)
 			.scope("message.read")
@@ -74,38 +78,29 @@ public class RedisRegisteredClientRepositoryTests {
 
 		redisRegisteredClientRepository.save(registeredClient);
 
-		JdbcRegisteredClientRepository jdbcRegisteredClientRepository = new JdbcRegisteredClientRepository(
-				jdbcTemplate);
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		httpHeaders.setBasicAuth(clientId, clientSecret);
+		MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+		requestBody.put("grant_type", Collections.singletonList("client_credentials"));
+		requestBody.put("scope", Collections.singletonList("openid profile message.read message.write"));
+		HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(requestBody, httpHeaders);
 
-		RegisteredClient findById = jdbcRegisteredClientRepository.findById(id);
-		assertNotNull(findById);
+		Map map = restTemplate.postForObject(String.format("http://127.0.0.1:%d/oauth2/token", serverPort), httpEntity,
+				Map.class);
 
-		RegisteredClient byClientId = jdbcRegisteredClientRepository.findByClientId(clientId);
-		assertNotNull(byClientId);
+		assertNotNull(map);
 
-		for (int i = 0; i < 2; i++) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
 
-			RegisteredClient findByIdRedis = redisRegisteredClientRepository.findById(id);
-			assertNotNull(findByIdRedis);
+		log.info("token:\n{}", objectWriter.writeValueAsString(map));
 
-			RegisteredClient findByClientIdRedis = redisRegisteredClientRepository.findByClientId(clientId);
-			assertNotNull(findByClientIdRedis);
-
-			ObjectMapper objectMapper = redisRegisteredClientRepository.getObjectMapper();
-
-			String idKey = redisRegisteredClientRepository.idKey(id);
-			String getById = stringRedisTemplate.opsForValue().get(idKey);
-			assertNotNull(getById);
-			RegisteredClient getByIdRegisteredClient = objectMapper.readValue(getById, RegisteredClient.class);
-			assertEquals(registeredClient, getByIdRegisteredClient);
-
-			String clientIdKey = redisRegisteredClientRepository.clientIdKey(clientId);
-			String getByClientId = stringRedisTemplate.opsForValue().get(clientIdKey);
-			assertNotNull(getByClientId);
-			RegisteredClient getByClientIdRegisteredClient = objectMapper.readValue(getByClientId,
-					RegisteredClient.class);
-			assertEquals(registeredClient, getByClientIdRegisteredClient);
-		}
+		assertNotNull(map.get("access_token"));
+		assertNotNull(map.get("scope"));
+		assertNotNull(map.get("token_type"));
+		assertNotNull(map.get("expires_in"));
 	}
 
 }
