e40245648a4c3ccc3c4f7fe0d1381843296bf250
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-03-21 13:46:46 +0800

    :bug: 优化 oauth2_registered_client 表数据储存

--- src/main/java/cn/com/xuxiaowei/boot/oauth2/service/RedisRegisteredClientRepository.java
+++ src/main/java/cn/com/xuxiaowei/boot/oauth2/service/RedisRegisteredClientRepository.java
@@ -79,7 +79,7 @@ public class RedisRegisteredClientRepository implements RegisteredClientReposito
 		String clientIdKey = clientIdKey(clientId);
 
 		stringRedisTemplate.opsForValue().set(idKey, json, timeout, TimeUnit.SECONDS);
-		stringRedisTemplate.opsForValue().set(clientIdKey, json, timeout, TimeUnit.SECONDS);
+		stringRedisTemplate.opsForValue().set(clientIdKey, id, timeout, TimeUnit.SECONDS);
 	}
 
 	@SneakyThrows
@@ -124,9 +124,10 @@ public class RedisRegisteredClientRepository implements RegisteredClientReposito
 		}
 
 		if (registeredClient != null) {
-			String clientIdKey = clientIdKey(registeredClient.getClientId());
+			String clientId = registeredClient.getClientId();
+			String clientIdKey = clientIdKey(clientId);
 
-			stringRedisTemplate.opsForValue().set(clientIdKey, json, timeout, TimeUnit.SECONDS);
+			stringRedisTemplate.opsForValue().set(clientIdKey, id, timeout, TimeUnit.SECONDS);
 		}
 
 		return registeredClient;
@@ -143,43 +144,70 @@ public class RedisRegisteredClientRepository implements RegisteredClientReposito
 		String redisVersion = RedisUtils.redisVersion(stringRedisTemplate);
 		int compare = StringUtils.compare(redisVersion, RedisConstants.GETDEL_VERSION);
 
-		String json;
+		String id;
 
+		// 查询 Redis 中的 id
 		if (compare < 0) {
 			log.warn("警告：Redis 版本低于 {}，不支持 GETEX（getAndExpire）命令", RedisConstants.GETEX_VERSION);
 
-			json = stringRedisTemplate.opsForValue().get(clientIdKey);
+			id = stringRedisTemplate.opsForValue().get(clientIdKey);
 
-			if (json != null) {
+			if (id != null) {
 				stringRedisTemplate.expire(clientIdKey, timeout, TimeUnit.SECONDS);
 			}
 		}
 		else {
-			json = stringRedisTemplate.opsForValue().getAndExpire(clientIdKey, timeout, TimeUnit.SECONDS);
+			id = stringRedisTemplate.opsForValue().getAndExpire(clientIdKey, timeout, TimeUnit.SECONDS);
 		}
 
-		RegisteredClient registeredClient;
+		if (id == null) {
+			// Redis 中 id 为空，查询数据库
 
-		if (json == null) {
-			registeredClient = jdbcRegisteredClientRepository.findByClientId(clientId);
+			RegisteredClient registeredClient = jdbcRegisteredClientRepository.findByClientId(clientId);
 
-			if (registeredClient != null) {
-				json = objectMapper.writeValueAsString(registeredClient);
-
-				stringRedisTemplate.opsForValue().set(clientIdKey, json, timeout, TimeUnit.SECONDS);
+			if (registeredClient == null) {
+				return null;
 			}
-		}
-		else {
-			registeredClient = objectMapper.readValue(json, RegisteredClient.class);
-		}
 
-		if (registeredClient != null) {
-			String idKey = idKey(registeredClient.getId());
+			id = registeredClient.getId();
+			String idKey = idKey(id);
+
+			String json = objectMapper.writeValueAsString(registeredClient);
 
 			stringRedisTemplate.opsForValue().set(idKey, json, timeout, TimeUnit.SECONDS);
+			stringRedisTemplate.opsForValue().set(clientIdKey, id, timeout, TimeUnit.SECONDS);
+
+			return registeredClient;
 		}
 
-		return registeredClient;
+		// Redis 中 id 不为空
+
+		if (compare < 0) {
+			log.warn("警告：Redis 版本低于 {}，不支持 GETEX（getAndExpire）命令", RedisConstants.GETEX_VERSION);
+
+			String idKey = idKey(id);
+
+			String json = stringRedisTemplate.opsForValue().get(idKey);
+
+			if (json == null) {
+				return null;
+			}
+
+			stringRedisTemplate.expire(idKey, timeout, TimeUnit.SECONDS);
+
+			return objectMapper.readValue(json, RegisteredClient.class);
+		}
+		else {
+			String idKey = idKey(id);
+
+			String json = stringRedisTemplate.opsForValue().getAndExpire(idKey, timeout, TimeUnit.SECONDS);
+
+			if (json == null) {
+				return null;
+			}
+
+			return objectMapper.readValue(json, RegisteredClient.class);
+		}
 	}
 
 	public String idKey(String id) {
